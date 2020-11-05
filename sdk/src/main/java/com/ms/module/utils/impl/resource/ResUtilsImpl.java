857f32c6ff7f8b9737518019689099013614c712
m_maohuawei <maohuawei@bdlbsc.com>
2020-11-05 11:37:58 +0800

    init

---
+++ sdk/src/main/java/com/ms/module/utils/impl/resource/ResUtilsImpl.java
@@ -0,0 +1,48 @@
+package com.ms.module.utils.impl.resource;
+
+import com.ms.module.supers.client.Modules;
+import com.ms.module.supers.inter.utils.IResUtilsAdapter;
+
+public class ResUtilsImpl extends IResUtilsAdapter {
+
+    public int getLayoutId(String paramString) {
+
+        return Modules.getDataModule().getAppData().getApplication().getResources().getIdentifier(paramString, "layout",
+                Modules.getDataModule().getAppData().getApplication().getPackageName());
+    }
+
+
+    public int getStringId(String paramString) {
+        return Modules.getDataModule().getAppData().getApplication().getResources().getIdentifier(paramString, "string",
+                Modules.getDataModule().getAppData().getApplication().getPackageName());
+    }
+
+    public int getDrawableId(String paramString) {
+        return Modules.getDataModule().getAppData().getApplication().getResources().getIdentifier(paramString,
+                "drawable", Modules.getDataModule().getAppData().getApplication().getPackageName());
+    }
+
+    public int getStyleId(String paramString) {
+        return Modules.getDataModule().getAppData().getApplication().getResources().getIdentifier(paramString,
+                "style", Modules.getDataModule().getAppData().getApplication().getPackageName());
+    }
+
+    public int getId(String paramString) {
+        return Modules.getDataModule().getAppData().getApplication().getResources().getIdentifier(paramString, "id", Modules.getDataModule().getAppData().getApplication().getPackageName());
+    }
+
+    public int getColorId(String paramString) {
+        return Modules.getDataModule().getAppData().getApplication().getResources().getIdentifier(paramString,
+                "color", Modules.getDataModule().getAppData().getApplication().getPackageName());
+    }
+
+    public int getArrayId(String paramString) {
+        return Modules.getDataModule().getAppData().getApplication().getResources().getIdentifier(paramString,
+                "array", Modules.getDataModule().getAppData().getApplication().getPackageName());
+    }
+
+    @Override
+    public String getString(int id) {
+        return Modules.getDataModule().getAppData().getApplication().getString(id);
+    }
+}
